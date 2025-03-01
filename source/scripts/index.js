947af7211cc59acf2d877a0e72f1ac4d7371675f
Владимир <10nva63@gmail.com>
2025-03-01 23:38:37 +0300

    1

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,17 @@
 /* в этот файл добавляет скрипты*/
+
+const navMain = document.querySelector('.main-nav'); /*находим навигацию -.main-nav */
+const navToggle = document.querySelector('.main-nav__toggle');/* нах. кнопку включения меню*/
+
+navMain.classList.remove('main-nav--nojs');/*удаляем класс при выключенном JS */
+
+/*на кнопку включения меню вешаем обработчик событий при клике на нее */
+navToggle.addEventListener('click', () => { // если при клике на кнпку
+  if (navMain.classList.contains('main-nav--closed')) { // в навигации есть этот класс
+    navMain.classList.remove('main-nav--closed'); // то удаляем этот класс (открываем меню)
+    navMain.classList.add('main-nav--opened'); // а в место ненего дабавляем другой класс
+  } else { // если нет данного класса
+    navMain.classList.add('main-nav--closed'); // добавляем его (закрываем меню)
+    navMain.classList.remove('main-nav--opened'); // и удаляем кдасс main-nav--opened
+  }
+});
